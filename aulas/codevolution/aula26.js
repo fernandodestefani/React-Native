3d86b7eba29bc2bbd36f578c66d20b40da48c9d2
Fernando Destefani <henriqueorei@gmail.com>
2024-09-07 09:18:12 -0300

    progresso do curso
    progresso do curso

--- aulas/codevolution/aula26.js
+++ aulas/codevolution/aula26.js
@@ -3,7 +3,6 @@ import Box from "./components/Box";
 
 export default function App() {
   return (
-    // <View style={{backgroundColor: 'plum', flex: 1}}></View>
     <View style={styles.container}>
       <Box style={{ backgroundColor: '#8e9b00'}}>Box 1</Box>
       <Box style={{ backgroundColor: '#b65d1f'}}>Box 2</Box>
@@ -11,16 +10,15 @@ export default function App() {
       <Box style={{ backgroundColor: '#ab9156'}}>Box 4</Box>
       <Box style={{ backgroundColor: '#6b0803'}}>Box 5</Box>
       <Box style={{ backgroundColor: '#1c4c56'}}>Box 6</Box>
-      <Box style={{ backgroundColor: '#b95f21'}}>Box 7</Box>  
+      <Box style={{ backgroundColor: '#b95f21'}}>Box 7</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     marginTop: 20,
     borderWidth: 6,
-    borderColor: 'red'
+    borderColor: 'red',
   },  
 });
--- aulas/codevolution/aula30.js
+++ aulas/codevolution/aula30.js
@@ -8,10 +8,10 @@ export default function App() {
       <Box style={{ backgroundColor: '#8e9b00'}}>Box 1</Box>
       <Box style={{ backgroundColor: '#b65d1f'}}>Box 2</Box>
       <Box style={{ backgroundColor: '#1c4c56'}}>Box 3</Box>
-      <Box style={{ backgroundColor: '#ab9156'}}>Box 4</Box>
+{/*       <Box style={{ backgroundColor: '#ab9156'}}>Box 4</Box>
       <Box style={{ backgroundColor: '#6b0803'}}>Box 5</Box>
       <Box style={{ backgroundColor: '#1c4c56'}}>Box 6</Box>
-      <Box style={{ backgroundColor: '#b95f21'}}>Box 7</Box>  
+      <Box style={{ backgroundColor: '#b95f21'}}>Box 7</Box> */}
     </View>
   );
 }
@@ -19,6 +19,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: "stretch",
     marginTop: 20,
     borderWidth: 6,
     borderColor: 'red'
