5f87b17af5a90fc85e8d739e8d5b90749ec250a8
Fernando Destefani <henriqueorei@gmail.com>
2024-10-11 18:45:24 -0300

    classes from today

--- App.js
+++ App.js
@@ -2,34 +2,27 @@ import {
   StyleSheet, 
   Text, 
   View, 
-  SectionList, 
-  ScrollView, 
   SafeAreaView, 
-  StatusBar
+  StatusBar,
+  TextInput
  } from "react-native";
-import pokemonList from './aulas/codevolution/data.json';
-import groupedPokemonList from './aulas/codevolution/grouped-data.json';
+import {useState} from "react";
 
 export default function App() {
+  const [name, setName] = useState("")
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.scrollView}>
-        <SectionList
-          sections={groupedPokemonList}
-          renderItem={({item}) => {
-            return (
-              <View style={styles.card}>
-                <Text style={styles.cardText}>{item}</Text>
-              </View>
-            )
-          }}
-          renderSectionHeader={({section}) => (
-          <Text style={styles.sectionHeaderText}>{section.type}</Text>
-        )}
-          ItemSeparatorComponent={() => <View style={{height: 16}}/>}
-          SectionSeparatorComponent={() => <View style={{height: 16}}/>}
-        />
-      </View>
+      <TextInput style={styles.input} 
+      value={name} 
+      onChangeText={setName}
+      placeholder='email@example.com'
+      secureTextEntry
+      keyboardType='numeric'
+      autoCorrect={false}
+      autoCapitalize='none'
+      />
+      <TextInput style={[styles.input, styles.multilineText]} placeholder='message' multiline/>
+      <Text style={styles.text}>My name is {name}</Text>
     </SafeAreaView>
   );
 }
@@ -37,36 +30,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#fff',
     paddingTop: StatusBar.currentHeight,
   },
-  scrollView: {
-    paddingHorizontal: 16,
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1
   },
-  card: {
-    backgroundColor: 'white',
-    padding: 16,
-    borderRadius: 8,
-    borderWidth: 1,
-    // marginBottom: 16,
-  },
-  cardText: {
+  text: {
     fontSize: 30,
+    padding: 10,
   },
-  headerText: {
-    fontSize: 24,
-    textAlign: 'center',
-    marginBottom: 12,
-  },
-  footerText: {
-    fontSize: 24,
-    textAlign: 'center',
-    marginTop: 12,
-    marginBottom: 12,
-  },
-  sectionHeaderText: {
-    backgroundColor: "white",
-    fontSize: 24,
-    fontWeight: 'bold'
+  multilineText: {
+    minHeight: 100,
+    textAlignVertical: 'top'
   }
 });
---
+++ aulas/codevolution/aula59.js
@@ -0,0 +1,45 @@
+import { 
+    StyleSheet, 
+    Text, 
+    View, 
+    SafeAreaView, 
+    StatusBar,
+    TextInput
+   } from "react-native";
+  import {useState} from "react";
+  
+  export default function App() {
+    const [name, setName] = useState("")
+    return (
+      <SafeAreaView style={styles.container}>
+        <TextInput style={styles.input} 
+        value={name} 
+        onChangeText={setName}
+        placeholder='email@example.com'
+        secureTextEntry
+        keyboardType='numeric'
+        autoCorrect={false}
+        autoCapitalize='none'
+        />
+        <Text style={styles.text}>My name is {name}</Text>
+      </SafeAreaView>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      paddingTop: StatusBar.currentHeight,
+    },
+    input: {
+      height: 40,
+      margin: 12,
+      padding: 10,
+      borderWidth: 1
+    },
+    text: {
+      fontSize: 30,
+      padding: 10,
+    }
+  });
