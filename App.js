cb12a7837604e3d0905b7aae5a62ea3f55b540a5
Fernando Destefani <henriqueorei@gmail.com>
2024-10-14 17:53:55 -0300

    classes from yesterday

--- App.js
+++ App.js
@@ -1,28 +1,71 @@
 import { 
-  StyleSheet, 
-  Text, 
-  View, 
   SafeAreaView, 
-  StatusBar,
-  TextInput
- } from "react-native";
-import {useState} from "react";
+  StyleSheet, 
+  StatusBar, 
+  View,
+  Text, 
+  ActivityIndicator,
+  FlatList } from "react-native";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const [name, setName] = useState("")
+
+  const [postList, setPostList] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [refreshing, setRefreshing] = useState(false)
+
+  const fetchData = async (limit = 10) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+    );
+    const data = await response.json()
+    setPostList(data)
+    setIsLoading(false);
+  }
+
+  const handleRefresh = () => {
+    setRefreshing(true)
+    fetchData(20)
+    setRefreshing(false)
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
+  if (isLoading) {
+    return (
+      <SafeAreaView style={styles.loadingContainer}>
+        <ActivityIndicator size={"large"} color='#0000ff'/>
+        <Text>Loading...</Text>
+      </SafeAreaView>
+    )
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput style={styles.input} 
-      value={name} 
-      onChangeText={setName}
-      placeholder='email@example.com'
-      secureTextEntry
-      keyboardType='numeric'
-      autoCorrect={false}
-      autoCapitalize='none'
-      />
-      <TextInput style={[styles.input, styles.multilineText]} placeholder='message' multiline/>
-      <Text style={styles.text}>My name is {name}</Text>
+      <StatusBar/>
+      <View style={styles.listContainer}>
+        <FlatList 
+        data={postList}
+        renderItem={({item}) => {
+          return (
+            <View style={styles.card}>
+              <Text style={styles.titleText}>{item.title}</Text>
+              <Text style={styles.bodyText}>{item.body}</Text>
+            </View>
+          )
+        }}
+        ItemSeparatorComponent={() => (
+          <View style={{height: 16}}/>
+        )}
+        ListEmptyComponent={<Text>No Posts Found</Text>}
+        ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
+        ListFooterComponent={<Text style={styles.footerText}>End of List</Text>}
+        refreshing={refreshing}
+        onRefresh={handleRefresh}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -30,21 +73,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: StatusBar.currentHeight,
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight
   },
-  input: {
-    height: 40,
-    margin: 12,
-    padding: 10,
-    borderWidth: 1
+  listContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+
   },
-  text: {
+  card: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  titleText: {
+    fontSize: 30
+  },
+  bodyText: {
+    fontSize: 24,
+    color: '#666666'
+  },
+  headerText: {
     fontSize: 30,
-    padding: 10,
+    textAlign: 'center',
+    marginBottom: 12,
+    fontWeight: 'bold'
   },
-  multilineText: {
-    minHeight: 100,
-    textAlignVertical: 'top'
+  footerText: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 12,
+    marginBottom: 12
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
-});
+})
