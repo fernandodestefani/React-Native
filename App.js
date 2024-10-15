8267a90a4bca3e6f51a67d0ab878ad70deb65797
Fernando Destefani <henriqueorei@gmail.com>
2024-10-15 20:00:03 -0300

    classes from today

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import {
   View,
   Text, 
   ActivityIndicator,
+  TextInput,
+  Button,
   FlatList } from "react-native";
 import { useState, useEffect } from "react";
 
@@ -13,20 +15,55 @@ export default function App() {
   const [postList, setPostList] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [refreshing, setRefreshing] = useState(false)
+  const [postTitle, setPostTitle] = useState('')
+  const [postBody, setPostBody] = useState('')
+  const [isPosting, setIsPosting] = useState(false)
+  const [error, setError] = useState('')
 
   const fetchData = async (limit = 10) => {
+    try{
     const response = await fetch(
       `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
     );
     const data = await response.json()
     setPostList(data)
     setIsLoading(false);
+    setError('')
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    setIsLoading(false)
+    setError('Failed to fetch post list')
   }
+  };
 
   const handleRefresh = () => {
     setRefreshing(true)
     fetchData(20)
     setRefreshing(false)
+  }
+
+  const addPost = async () => {
+    setIsPosting(true)
+    try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'post',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        title: postTitle,
+        body: postBody
+
+      })
+    })
+    const newPost = await response.json()
+    setPostList([newPost, ...postList])
+    setPostTitle('')
+    setPostBody('')
+    setIsPosting(false)
+    setError('')
+  } catch(error) {
+    console.error('Error adding new post:', error)
+    setError('Failed to add new post')
+  }
   }
 
   useEffect(() => {
@@ -45,6 +82,27 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar/>
+      {error ? (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{error}</Text>
+        </View>
+      ) : (
+      <>
+      <View style={styles.inputContainer}>
+        <TextInput
+         style={styles.input} 
+         placeholder="Post title" 
+         value={postTitle} 
+         onChangeText={setPostTitle}
+        />
+        <TextInput
+          style={styles.input} 
+          placeholder="Post body" 
+          value={postBody} 
+          onChangeText={setPostBody}
+        />
+        <Button title={isPosting ? 'Adding...' : 'Add Post'} onPress={addPost}disabled={isPosting}/>
+      </View>
       <View style={styles.listContainer}>
         <FlatList 
         data={postList}
@@ -66,6 +124,8 @@ export default function App() {
         onRefresh={handleRefresh}
         />
       </View>
+      </>
+      )}
     </SafeAreaView>
   );
 }
@@ -111,5 +171,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5F5F5',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  inputContainer: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+    borderRadius: 8,
+  },
+  errorContainer: {
+    backgroundColor: '#FFC0CB',
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+    alignItems: 'center'
+  },
+  errorText: {
+    color: '#D8000C',
+    fontSize: 16,
+    textAlign: 'center'
   }
 })
