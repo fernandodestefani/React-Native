76f6989801e98957642d81e5e3fd2ef4e3ae106f
Fernando Destefani <henriqueorei@gmail.com>
2024-10-20 08:25:53 -0300

    classes from yesterday

---
+++ screens/AboutScreen.js
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet, Button } from "react-native";
+
+export default function AboutScreen({ route, navigation }) {
+    const {name} = route.params
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>About {name}</Text>
+            <Button title="Update the name" onPress={() => navigation.setParams({
+                name: 'Noobies'
+            })}/>
+            <Button title="Go back with data"
+            onPress={() => navigation.navigate('Home', {result: 'Data from about'})}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+    }
+})
