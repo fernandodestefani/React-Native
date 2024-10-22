dd27ef1ade79cb75edbf7ada94bd073beabc2ddd
Fernando Destefani <henriqueorei@gmail.com>
2024-10-22 19:35:58 -0300

    classes from today

--- App.js
+++ App.js
@@ -1,30 +1,35 @@
-import 'react-native-gesture-handler'
-import { NavigationContainer } from '@react-navigation/native'
-import { createDrawerNavigator } from '@react-navigation/drawer'
-import DashboardScreen from './screens/DashboardScreen'
-import SettingsScreen from './screens/SettingsScreen'
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ProfileScreen from "./screens/ProfileScreen";
+import CourseListScreen from "./screens/CourseListScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import Ionicons from "@expo/vector-icons/Ionicons"
 
-const Drawer = createDrawerNavigator()
+const Tab = createBottomTabNavigator()
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Drawer.Navigator>
-                <Drawer.Screen
-                 name='Dashboard'
-                 component={DashboardScreen}
-                 options={{
-                    title: 'My Dashboard',
-                    drawerLabel: 'Dashboard Label',
-                    drawerActiveTintColor: '#333',
-                    drawerActiveBackgroundColor: 'lightblue',
-                    drawerContentStyle: {
-                        backgroundColor: '#c6cbef',
-                    }
-                 }}
-                />
-                <Drawer.Screen name='Settings' component={SettingsScreen}/>
-            </Drawer.Navigator>
+            <Tab.Navigator screenOptions={{
+                tabBarLabelPosition: 'beside-icon',
+                tabBarShowLabel: true,
+                tabBarActiveTintColor: 'blue',
+                tabBarInactiveTintColor: 'grey'
+            }}>
+                <Tab.Screen name="Course List" component={CourseListScreen} options={{
+                    tabBarLabel: 'Course List',
+                    tabBarIcon: ({color}) => (<Ionicons name="list" size={25} color={color}/>)
+                }}/>
+                <Tab.Screen name="Profile" component={ProfileScreen} options={{
+                    tabBarLabel: 'My Profile',
+                    tabBarIcon: ({color}) => (<Ionicons name="person" size={20} color={color}/>),
+                    tabBarBadge: 3
+                }}/>
+                <Tab.Screen name="Settings" component={SettingsScreen} options={{
+                    tabBarLabel: 'Settings',
+                    tabBarIcon: ({color}) => (<Ionicons name="settings" size={25} color={color}/>)
+                }}/>
+            </Tab.Navigator>
         </NavigationContainer>
     )
 }
